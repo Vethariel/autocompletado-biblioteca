ad44e947404be337603210c3dab0241d2a10b619
automas-procesos-gth <analista.procesos@cdaautomas.com.co>
2025-06-16 10:12:17 -0500

    Estructura inicial del proyecto de autocompletado

---
+++ main.js
@@ -0,0 +1,6 @@
+import { SearchInput } from './src/components/SearchInput.js';
+import { SuggestionsPanel } from './src/components/SuggestionsPanel.js';
+import { RankingPanel } from './src/components/RankingPanel.js';
+import { TimeChart } from './src/components/TimeChart.js';
+
+// Inicialización y lógica principal aquí
