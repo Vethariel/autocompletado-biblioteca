4d35c1b1491e5177d837b51831f601860fd1f30d
LoytDaniel <dloyarias@gmail.com>
2025-06-26 17:47:48 -0500

    Implementacion de AVL

---
+++ src/structures/AVLTree.js
@@ -0,0 +1,16 @@
+class AVLNode {
+    constructor(valor) {
+        this.valor = valor;
+        this.altura = 1;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+}
+
+class AVLTree {
+    constructor() {
+        this.raiz = null;
+    }
+
+    // Métodos para insertar, eliminar y buscar
+}
