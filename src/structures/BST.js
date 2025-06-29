15acfdc071e6ffc4c5ee28a05b4370e6aa1287b4
LoytDaniel <dloyarias@gmail.com>
2025-06-29 02:29:43 -0500

    Intancia de BST

---
+++ src/structures/BST.js
@@ -0,0 +1,14 @@
+class BSTNodo {
+    constructor(value) {
+        this.valor = valor;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.raiz = null;
+    }
+    
+}
